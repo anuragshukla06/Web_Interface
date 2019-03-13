af5d7a6f2bccd88892c0f53b228666bffad08fcf
Anurag Shukla <shukla.anurag0006@gmail.com>
2019-03-13 20:29:33 +0530

    created homepage (no content)

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Anurag Shukla\Desktop\Mini Project\Web_Interface\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
