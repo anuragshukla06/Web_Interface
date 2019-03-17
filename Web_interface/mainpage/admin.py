a164d37583f618e7aafc70063340613b7279cfb3
Anurag Shukla <shukla.anurag0006@gmail.com>
2019-03-17 10:50:28 +0530

    Created models

--- Web_interface/mainpage/admin.py
+++ Web_interface/mainpage/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import PreSavedData, Entry
 # Register your models here.
+admin.site.register(PreSavedData)
+admin.site.register(Entry)
--- Web_interface/mainpage/models.py
+++ Web_interface/mainpage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    start_date = models.DateTimeField(auto_now_add=True);
+    end_date = models.DateTimeField(auto_now=False, blank=True);
+    fruit = models.CharField(max_length=200);
+    running = models.BooleanField(default=False);
+
+    def __str__(self):
+        print(self.fruit + " " + str(self.start_date) + " " + str(self.end_date) +" "+ str(self.running))
+
+class PreSavedData(models.Model):
+    fruit = models.CharField(max_length=200);
+    temperature = models.IntegerField();
+    relative_humidity = models.IntegerField();
+    numberOfDays = models.IntegerField();
